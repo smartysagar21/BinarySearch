b9417ad0a2915f4a6dbe4eaea5182ef4b2f07e38
Shwet Sagar <105374331+smartysagar21@users.noreply.github.com>
2022-07-17 00:49:03 +0530

    Add files via upload

---
+++ 01_ToFindAnElement.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int binary_search(vector<int> &v, int element){
+	int lo=0, hi=v.size()-1;
+  
+  	while(hi>=lo){
+		int mid=lo+(hi-lo)/2;
+  
+  		if(v[mid]==element){
+			return mid;
+		}else if(v[mid]>element){
+    		hi=mid-1;
+  		}else{
+    		lo=mid+1;
+  		}
+  	}
+	  
+	return -1;
+}
+
+int main() {
+  	int n;
+  	cin>>n;
+  
+  	vector<int> v(n);
+  	for(int i=0; i<n; i++){
+    	cin>>v[i];
+  	}
+  
+  	int to_find;
+  	cin>>to_find;
+  
+  	cout<<binary_search(v,to_find)<<endl;
+  
+  	return 0;
+}
